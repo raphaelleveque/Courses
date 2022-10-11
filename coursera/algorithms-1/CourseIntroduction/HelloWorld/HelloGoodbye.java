f176641a3fa54ef45023d8f484f34251d220c31f
Raphael Leveque <raphaellev1102@gmail.com>
2022-10-11 09:26:30 -0300

    coursera and udemy

---
+++ coursera/algorithms-1/CourseIntroduction/HelloWorld/HelloGoodbye.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+		System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+	}
+}
