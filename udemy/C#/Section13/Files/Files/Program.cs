5237435f09ef74c18c7e0e3f3860adaf5a300c77
Raphael Leveque <raphaellev1102@gmail.com>
2023-09-28 17:57:05 -0300

    curso de c# section 14

--- udemy/C#/Section13/Files/Files/Program.cs
+++ udemy/C#/Section13/Files/Files/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace Files;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+class Program {
+    static void Main(string[] args) {
+        string directory = "/Users/raphaelleveque/Desktop/cs/courses/udemy/C#/Section13/";
+        string filename = "itens.csv";
+        string path = directory + filename;
+
+        string newDirectory = directory + "out/";
+        string newFilePath = directory + "out/summary.csv";
+        if (!Directory.Exists(newDirectory)) {
+            Directory.CreateDirectory(newDirectory);
+        }
+
+        using (StreamReader reader = new StreamReader(path)) {
+            using (StreamWriter writer = new StreamWriter(newFilePath)) {
+                reader.ReadLine();
+                string? line;
+                while ((line = reader.ReadLine()) != null) {
+                    string[] csvfile = line.Split(',');
+                    writer.Write(csvfile[0] + ",");
+                    double valorFinal = double.Parse(csvfile[1]) * int.Parse(csvfile[2]);
+                    writer.Write(valorFinal.ToString("F2") + "\n");
+
+                }
+            }
+        }
     }
 }
 
---
+++ udemy/Section14/208Interfaces/208Interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _208Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
